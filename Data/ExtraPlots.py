6c8c73980a1a35ec0ad6b6cbea85c811640f15d7
oscarlomborg <s213935@dtu.dk>
2026-06-07 22:19:26 +0200

    Error differences plots

--- Data/ExtraPlots.py
+++ Data/ExtraPlots.py
@@ -34,6 +34,30 @@ ax[1].legend()
 
 # %%
 
+fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+
+ax[0].plot(seeds, data_moe["L_max"] - data_pinn["L_max"], label=r"Difference (MoE-PINN)")
+ax[0].axhline(np.mean(data_moe["L_max"] - data_pinn["L_max"]),
+     linestyle="--", label=fr"Mean difference: {np.mean(data_moe['L_max'] - data_pinn['L_max']):.2e}")
+ax[0].axhline(0, color="red")
+ax[0].set_xlabel("Seed")
+ax[0].set_ylabel(r"$L_\infty$ error difference")
+ax[0].set_title(r"$L_\infty$ difference vs seed")
+ax[0].grid(True)
+ax[0].legend()
+
+ax[1].plot(seeds, data_moe["L_2"] - data_pinn["L_2"], label=r"Difference (MoE-PINN)")
+ax[1].axhline(np.mean(data_moe["L_2"] - data_pinn["L_2"]),
+     linestyle="--", label=fr"Mean difference: {np.mean(data_moe['L_2'] - data_pinn['L_2']):.2e}")
+ax[1].axhline(0, color="red")
+ax[1].set_xlabel("Seed")
+ax[1].set_ylabel(r"$L_2$ error difference")
+ax[1].set_title(r"$L_2$ difference vs seed")
+ax[1].grid(True)
+ax[1].legend()
+
+# %%
+
 plt.tight_layout()
 plt.show()
 
