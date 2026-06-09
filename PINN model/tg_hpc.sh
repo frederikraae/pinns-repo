ed328891994df40785c6a8f9f6158a8ef9e7bf57
oscarlomborg <s213935@dtu.dk>
2026-06-09 22:35:13 +0200

    100 collacations

--- PINN model/tg_hpc.sh
+++ PINN model/tg_hpc.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-#BSUB -J tg_hpc
+#BSUB -J tg_hpc_100
 #BSUB -q hpc
 #BSUB -n 1
-#BSUB -W 04:00
+#BSUB -W 00:30
 #BSUB -R "span[hosts=1]"
-#BSUB -R "rusage[mem=2GB]"
-#BSUB -B
-#BSUB -N
-#BSUB -o pinn_hpc_%J.out
-#BSUB -e pinn_hpc_%J.err
+#BSUB -R "rusage[mem=1GB]"
+#BSUB -o tg_hpc_%J.out
+#BSUB -e tg_hpc_%J.err
 
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate PINN
 
-python -u taylorgreen.py 5000
+python -u taylorgreen.py 100
