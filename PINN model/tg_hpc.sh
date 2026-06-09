838b138ff7b0122a0a6a3c943d84c2adc7691c38
oscarlomborg <s213935@dtu.dk>
2026-06-09 21:37:02 +0200

    First results

--- PINN model/tg_hpc.sh
+++ PINN model/tg_hpc.sh
@@ -2,9 +2,11 @@
 #BSUB -J tg_hpc
 #BSUB -q hpc
 #BSUB -n 1
-#BSUB -W 00:30
+#BSUB -W 04:00
 #BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=2GB]"
+#BSUB -B
+#BSUB -N
 #BSUB -o pinn_hpc_%J.out
 #BSUB -e pinn_hpc_%J.err
 
@@ -12,4 +14,4 @@
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate PINN
 
-python -u taylorgreen.py 200
+python -u taylorgreen.py 5000
